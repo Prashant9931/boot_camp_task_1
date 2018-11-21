ade540d1463bda453527caac22e70c45c31d741d
Prashant9931 <tprashant665@gmail.com>
2018-11-21 00:05:38 -0800

    Add files via upload

---
+++ models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+
+class Movie(models.Model):
+    actor=models.CharField(max_length=30)
+    actor_movie=models.CharField(max_length=50)
+    genre=models.CharField(max_length=20)
+    movie_logo=models.CharField(max_length=100)
+
+
+class Song(models.Model):
+    movie=models.ForeignKey(Movie,on_delete=models.CASCADE)
+    file_type=models.CharField(max_length=50)
+    song_name=models.CharField(max_length=100)
+
+
+
+# Create your models here.
